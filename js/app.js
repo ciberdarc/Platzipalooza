7fa2c7ceaa8d25c9fe15dae9797e67a9b39ef11a
Alexis Araujo <enrique.a.lopez@nielsen.com>
2016-10-17 17:01:28 -0500

    Menu offcanvas con foundation

--- js/app.js
+++ js/app.js
@@ -6,3 +6,12 @@ $('#lineup-tabs').foundation('selectTab', $(this).data('toggleDia'))
 
 });
 
+const $offCanvas = $('#offCanvas')
+const $sticky = $('#sticky')
+
+$offCanvas.find('li').click(function (ev){
+	$offCanvas.foundation('close')
+	setTimeout(function(){
+		$sticky.css('left', '0px')	
+	}, 300)
+})
